7f13388e78c773d2a72b969ec3f692ec87bd1684
Fathima <fathimanaiha03@gmail.com>
2024-11-16 23:19:53 +0530

    Private folders completed

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,4 @@
+export default function PrivateRoute()
+{
+    return <h1>You cannot view this page in the browser</h1>
+}
--- src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,3 +1,9 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({params} : {params: {productid : string; reviewid : string;}}){
+    if(parseInt(params.reviewid)>1000)
+    {
+        notFound();
+    }
     return <h1>Review {params.reviewid} for product {params.productid}</h1>;
 } 
