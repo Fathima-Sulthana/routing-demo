13c76b0f884e3e1a36ac00eb35515b9da6a0f526
Fathima <fathimanaiha03@gmail.com>
2024-11-24 21:52:16 +0530

    error handling

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,3 +1,18 @@
-export default function ProductDetails({params} : {params: {productid : string}}){
+import { Metadata } from "next";
+
+type Props = {params : {productid: string;}} 
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const title= await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productid}`)
+        }, 100)
+    }) 
+    return {
+        title: `Product ${title}`
+    }
+}
+
+export default function ProductDetails({params} : Props){
     return <h1>Details about product {params.productid}</h1>;
 }
--- src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,6 +1,16 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count:number){
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({params} : {params: {productid : string; reviewid : string;}}){
+    const random = getRandomInt(2)
+    if(random == 1)
+    {
+        throw new Error("Error loading review");
+    }
+
     if(parseInt(params.reviewid)>1000)
     {
         notFound();
